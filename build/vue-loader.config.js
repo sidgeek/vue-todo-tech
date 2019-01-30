fb70491481d133a54e8867df1d9849a1d53a9aef
sidgeek <sid.shi@nirvana-info.com>
2019-01-31 00:52:55 +0800

    2-2: vue-loader config

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,6 +1,18 @@
+// const docLoader = require.resolve('./doc-loader')
 module.exports = (isDev) => {
   return {
     preservewhitepace: true,  //去除vue组件中不小心加入的空格
-    extractCSS: isDev
+    extractCSS: !isDev,
+    cssModules: {},
+    // hotReload: false, // 根据环境变量生成
+    // loaders: {
+    //   'docs': docLoader,
+    //   'js': 'coffe-loader',
+    //   // html, style
+    // }, // 自定义组件
+    // preLoader: {
+    // }, // 
+    // postLoader: {
+    // }
   }
 }
