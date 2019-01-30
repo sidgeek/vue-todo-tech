6e66c49186f1b13e0fb0eb09b91fb9514aa1f401
sidgeek <sid.shi@nirvana-info.com>
2019-01-31 00:38:51 +0800

    2-2: add npm run clean

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,6 +1,6 @@
 module.exports = (isDev) => {
   return {
-    preservewhitepace: ture,  //去除vue组件中不小心加入的空格
-    extractCSS: true
+    preservewhitepace: true,  //去除vue组件中不小心加入的空格
+    extractCSS: isDev
   }
 }
