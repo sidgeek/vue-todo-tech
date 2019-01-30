5f7552acef9f9beba8083bd7d81e3ac3f3698824
sidgeek <sid.shi@nirvana-info.com>
2019-01-31 00:06:38 +0800

    2-1: Adjust directory structure

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -6,7 +6,7 @@ const config = {
   entry: path.join(__dirname, '../src/index.js'),
   output: {
     filename: 'bundle.[hash:8].js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, '../dist')
   },
   module: {
     rules: [
